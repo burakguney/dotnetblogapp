842d14f4248f9a5f0d06fa0ec0b7d1e3ebe4b4a6
Burak Guney <burakguney@outlook.com.tr>
2023-12-31 16:50:31 +0300

    initial commit

---
+++ BlogApp/BlogApp/Controllers/PostsController.cs
@@ -0,0 +1,30 @@
+using BlogApp.Data.Abstract;
+using BlogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        private IPostRepository _postRepository;
+        public PostsController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(
+                new PostsViewModel 
+                {
+                    Posts = _postRepository.Posts.ToList()
+                }
+            );
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            return View(await _postRepository.Posts.FirstOrDefaultAsync(p => p.PostId == id));
+        }
+    }
+}
